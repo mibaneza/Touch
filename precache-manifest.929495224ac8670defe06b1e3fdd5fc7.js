2fc49a739db1baffff9e76666a9fd1925d6c9c07
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-02-05 14:10:01 -0500

    gaaaa
    izi

---
+++ precache-manifest.929495224ac8670defe06b1e3fdd5fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6d6b3788942eedeb80a66d9013e337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8363deaeebcae088c563",
+    "url": "/static/css/main.37551811.chunk.css"
+  },
+  {
+    "revision": "34b49a8fa10d2454c938",
+    "url": "/static/js/2.9e9327d7.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/static/js/2.9e9327d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8363deaeebcae088c563",
+    "url": "/static/js/main.c6a8ffd6.chunk.js"
+  },
+  {
+    "revision": "e05489611550127c8e89",
+    "url": "/static/js/runtime-main.6ea15898.js"
+  }
+]);
